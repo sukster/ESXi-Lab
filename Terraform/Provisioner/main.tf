40182c3069eb40640abac79af806b2f41dc1be36
Ludek Suk <ludek.suk@gmail.com>
2020-11-02 12:40:41 +0100

    Updates to Terraform main file

--- Terraform/Provisioner/main.tf
+++ Terraform/Provisioner/main.tf
@@ -49,20 +49,20 @@ resource "esxi_guest" "provisioner" {
   network_interfaces {
     virtual_network = var.vm_network
 #    mac_address     = "00:50:56:a3:b1:c2"
-    nic_type        = "e1000"
+    nic_type        = "vmxnet3"
   }
   # This is the local network that will be used for 192.168.38.x addressing
   network_interfaces {
     virtual_network = var.hostonly_network
 #    mac_address     = "00:50:56:a3:b1:c4"
-    nic_type        = "e1000"
+    nic_type        = "vmxnet3"
   }
   # OPTIONAL: Uncomment out this interface stanza if your vm_network doesn't 
   # provide internet access
   # network_interfaces {
   #  virtual_network = var.nat_network
   #  mac_address     = "00:50:56:a3:b1:c3"
-  #  nic_type        = "e1000"
+  #  nic_type        = "vmxnet3"
   # }
   guest_startup_timeout  = 45
   guest_shutdown_timeout = 30
