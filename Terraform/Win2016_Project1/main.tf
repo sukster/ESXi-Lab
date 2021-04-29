40b1993dbdf17ea0dccef4e1dc2e51539d7cf957
Ludek Suk <ludek.suk@gmail.com>
2021-04-29 12:57:40 +0000

    Updates

--- Terraform/Win2016_Project1/main.tf
+++ Terraform/Win2016_Project1/main.tf
@@ -17,7 +17,7 @@ provider "esxi" {
 
 resource "esxi_guest" "win2016srv1" {
   guest_name = "win2016srv1"
-  disk_store = "Local_NVMe"
+  disk_store = "Local_NVMe (2)"
   guestos    = "windows9srv-64"
 
   boot_disk_type = "thin"
@@ -34,12 +34,14 @@ resource "esxi_guest" "win2016srv1" {
  #   mac_address     = "00:50:56:a2:b1:c2"
     nic_type        = "vmxnet3"
   }
-  # This is the local network that will be used for 192.168.38.x addressing
-  network_interfaces {
-    virtual_network = var.hostonly_network
+
+ # This is the local network that will be used for 192.168.38.x addressing
+ # network_interfaces {
+ #   virtual_network = var.hostonly_network
  #   mac_address     = "00:50:56:a2:b1:c4"
-    nic_type        = "vmxnet3"
-  }
+ #   nic_type        = "vmxnet3"
+ # }
+ 
   guest_startup_timeout  = 45
   guest_shutdown_timeout = 30
 }
--- Terraform/Win2016_Project1/variables.tf
+++ Terraform/Win2016_Project1/variables.tf
@@ -4,7 +4,7 @@
 #  Change these defaults to fit your needs!
 
 variable "esxi_hostname" {
-  default = "10.10.10.2"
+  default = "10.10.10.3"
 }
 
 variable "esxi_hostport" {
@@ -20,8 +20,4 @@ variable "esxi_password" { # Unspecified will prompt
 
 variable "vm_network" {
   default = "VM Network"
-}
-
-variable "hostonly_network" {
-  default = "HostOnly Network"
 }
